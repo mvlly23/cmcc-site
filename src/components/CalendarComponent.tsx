d8dfd340e5f81e7054154930f12b6371e08ae6db
Molly <mollybraswell232@gmail.com>
2024-06-04 15:29:50 -0400

    Fixed errors
    Added spacer before footer on events page

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -5,7 +5,7 @@ import { Calendar } from "@/components/ui/calendar"
 export default function CalendarComponent() {
     const selectedDate = new Date();
     const dates = [1, 12, 26, 27, 30];
-    var arrayMatcher: Matcher = [];
+    var arrayMatcher = [];
     const daysInMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 0).getDate();
     for (var i = 0; i < daysInMonth; i++) {
         const date = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), i);
@@ -18,15 +18,15 @@ export default function CalendarComponent() {
         <div className="calendar-events grid justify-items-center">
             <div className="calendar-component">
                 <Calendar
-                mode="none"
-                disableNavigation="true"
+                mode="default"
+                disableNavigation={ true }
                 selected={ selectedDate }
                 disabled={ arrayMatcher }
                 className="rounded-md border bg-white"
                 />
             </div>
             <div className="events">
-                <ul class="list-disc list-outside m-2 ml-6">
+                <ul className="list-disc list-outside m-2 ml-6">
                     <li>June 1st - Palestine Rally</li>
                     <li>June 12th - Flyering</li>
                     <li>June 26th - Tabeling</li>
